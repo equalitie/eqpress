02b020d241fc09ce0275de4dac2ac2f1680d485b
Victor Granic <eqpress@boreal321.com>
2015-06-23 22:39:11 -0400

    Replace some more easypress text in the Console code.

--- files/var/www/easypress.ca/console/plugin/easypress-console/inc/easypress-console-text.php
+++ files/var/www/easypress.ca/console/plugin/easypress-console/inc/easypress-console-text.php
@@ -21,9 +21,9 @@ function ep_console_docs_website_stats() {
 					if ( false !== $on_the_overview_page ) {
 ?>
 						<div class="postbox">
-							<h3><span>About the easyPress Console</span></h3>
+							<h3><span>About The Console</span></h3>
 							<div class="inside">
-								<p>The easyPress Console allows you to perform some administrative tasks that otherwise would require shell access.</p>
+								<p>The Console allows you to perform some administrative tasks that otherwise would require shell access.</p>
 							</div>
 						</div> <!-- .postbox -->
 <?php
@@ -101,7 +101,7 @@ function ep_console_docs_file_permissions() {
 					<div class="postbox">
 						<h3><span>File Permissions</span></h3>
 						<div class="inside">
-							<p>The File Permissions feature allows you to reset the permissions and ownership on your files back to the default settings. The reason you might need to do this is if you upload or install a plugin manually and then that plugin requires write access to functions properly. The issue is that your SFTP username is not the same as the web server's user name so when you upload files into your document root you do so as your SFTP username. So, when the plugin tries to write to a file or directory that's owned by your SFTP username and not the web server's username it will fail because of insufficient ownership and permission of those files. The button on the easyPress File Permissions page will reset all directories and files under your document root to be owned by the web server user.</p>
+							<p>The File Permissions feature allows you to reset the permissions and ownership on your files back to the default settings. The reason you might need to do this is if you upload or install a plugin manually and then that plugin requires write access to functions properly. The issue is that your SFTP username is not the same as the web server's user name so when you upload files into your document root you do so as your SFTP username. So, when the plugin tries to write to a file or directory that's owned by your SFTP username and not the web server's username it will fail because of insufficient ownership and permission of those files. The button on the File Permissions page will reset all directories and files under your document root to be owned by the web server user.</p>
 							<p>This default setting is very convenient for installing and updating plugins and themes but it's not the most secure way to configure a WordPress environment. This is why we also provide the Security Lockdown feature.
 						</div> <!-- .inside -->
 					</div> <!-- .postbox -->
@@ -186,7 +186,7 @@ function ep_console_docs_wpadmin_ssl() {
 								<li>Enable SSL for logging in only.</li>	
 								<li>Disable SSL.</li>	
 							</ol></p>
-							<p>Please note that if you enable SSL you will be using the easyPress SSL certificates therefore you will encounter SSL warnings the first time you visit your admin screens. You can read more about the warnings you will encounter on the easyPress help page titled <a href="http://easypress.ca/security-warning-for-wp-admin-and-ssl-certificates/" target="_blank" title="Security warning for wp-admin and SSL certificates">Security warning for wp-admin and SSL certificates.</a> If you have your own SSL certificates please get in touch with <a href="mailto:support@easypress.ca">easyPress Support</a> and we will install them for you.</p>
+							<p>Please note that if you enable SSL you will be using the our SSL certificates therefore you will encounter SSL warnings the first time you visit your admin screens. You can read more about the warnings you will encounter on the help page titled <a href="http://eqpress.equalit.ie/security-warning-for-wp-admin-and-ssl-certificates/" target="_blank" title="Security warning for wp-admin and SSL certificates">Security warning for wp-admin and SSL certificates.</a> If you have your own SSL certificates please get in touch with <a href="mailto:{{ org_support_email }}">Support</a> and we will install them for you.</p>
 							<p>You can read more about <a href="http://codex.wordpress.org/Administration_Over_SSL" target="_blank" title="Administration Over SSL for WordPress">Administration Over SSL</a> on the WordPress Codex</p>	
 					</div> <!-- .inside -->
 					</div> <!-- .postbox -->
