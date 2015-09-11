4f823ee994ca2b5e50c3f323ad3b056ab514a7d9
KheOps <kheops@ceops.eu>
2015-09-12 01:38:47 +0200

    Rewrite check_for_disk_usage.sh for a more systematic check of all partitions.
    Signed-off-by: KheOps <kheops@ceops.eu>

--- files/usr/local/sbin/check_for_disk_usage.sh
+++ files/usr/local/sbin/check_for_disk_usage.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/usr/local/sbin:/bin:/usr/bin:/sbin:/usr/sbin
-disku=`df -h|head -2|tail -1|awk '{print $5}'|sed -e 's/%//'`
-if [ "$disku" -gt 95 ]; then
-	df -h | mail -s "Disk is low on $(hostname)" sysadmin@easypress.ca
+THRESHOLD=50
+WARNPARTS=$(
+df -h | tail -n +2 | while read line; do
+  MOUNTPOINT=$(echo $line | cut -d' ' -f 6-)
+  FILLPC=$(echo $line | cut -d' ' -f 5 | sed 's/%$//')
+  [ $FILLPC -gt $THRESHOLD ] && echo -n "$MOUNTPOINT, "
+done
+)
+WARNPARTS=${WARNPARTS%??}
+
+if [ -n "$WARNPARTS" ]; then
+  df -h | mail -s "Partitions with low disk space on $(hostname): $WARNPARTS" sysadmin@easypress.ca
 fi
