8cddaefe5f19ee06f0c79d469321c36da9084ce2
KheOps <kheops@ceops.eu>
2015-09-14 20:08:17 +0200

    top_ram_procs.sh: remove call to 'field' script (replace with actual awk command)
    Signed-off-by: KheOps <kheops@ceops.eu>

--- files/usr/local/sbin/top_ram_procs.sh
+++ files/usr/local/sbin/top_ram_procs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-top -bn1  | grep '^[ 0-9]' | field 1 6 NF | grep '[mg] ' | sed 's/\.\([0-9]\)g/\100m/' | awk '{m[$3]+=$2} END {for (a in m) { print a, m[a] }}' | sort -k2rn
+top -bn1  | grep '^[ 0-9]' | awk '{print $1, $6, $NF}' | grep '[mg] ' | sed 's/\.\([0-9]\)g/\100m/' | awk '{m[$3]+=$2} END {for (a in m) { print a, m[a] }}' | sort -k2rn
