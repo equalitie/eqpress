fe40a0ca379ff03c6ba73ae00dbefae2eb79ef7a
KheOps <kheops@ceops.eu>
2015-09-14 19:28:56 +0200

    monit_php-fpm_procs.sh: put threshold in variable
    Signed-off-by: KheOps <kheops@ceops.eu>

--- files/usr/local/sbin/monit_php-fpm_procs.sh
+++ files/usr/local/sbin/monit_php-fpm_procs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PATH=/bin:/usr/bin:/usr/local/sbin
+PSLIMIT=14
 PS=`ps ax|grep php-fpm|grep www|wc -l`
 hostname=`hostname`
-if [ "$PS" -gt 14 ]; then
+if [ "$PS" -gt $PSLIMIT ]; then
 	echo "warning: php-fpm www pool has reached $PS procs on ${hostname}"
 	exit 1
 else
