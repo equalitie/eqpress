1921e9c0769922542093cb564630d26f9a07f163
Adriano Cataluddi <acataluddi@gmail.com>
2016-02-19 21:58:43 +0100

    Removed references to external entities, IP and companies.

--- files/usr/local/bin/currentconnections.sh
+++ files/usr/local/bin/currentconnections.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
 # Reports current network connection numbers to mrtg
-# mf@in-tuition.net
 
 TCP=`cat /proc/net/nf_conntrack | grep tcp | wc -l`
 UDP=`cat /proc/net/nf_conntrack | grep udp | wc -l`
