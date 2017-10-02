ce34ea58f33169786ea6baca95c5e19ed76a0ae3
RamJett <ramjett@ramnic.com>
2017-10-02 20:38:19 +0000

    db name needed on export because of wp cli upgrade

--- files/usr/local/sbin/wp_db_backup.sh
+++ files/usr/local/sbin/wp_db_backup.sh
@@ -5,7 +5,7 @@ for docroot in /var/www/*/wordpress; do
 	cd $docroot
 	dbinfo=($(grep DB_NAME ${docroot}/wp-config.php |tr "'" '\n'))
 	dbname=${dbinfo[3]}
-	wp --allow-root db export || mysqldump ${dbname} > ${dbname}.sql
+	wp --allow-root db export ${dbname}.sql || mysqldump ${dbname} > ${dbname}.sql
 	gzip ${dbname}.sql
 	mv ${dbname}.sql.gz ../
 	echo
