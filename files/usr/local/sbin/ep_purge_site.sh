408f3b0fd3c664c075adf7ec5c52d31f2e578303
RamJett <ramjett@ramnic.com>
2019-06-05 20:47:11 +0000

    remove unneeded cache.conf copy

--- files/usr/local/sbin/ep_purge_site.sh
+++ files/usr/local/sbin/ep_purge_site.sh
@@ -16,9 +16,6 @@ purge() {
         rm -r /var/www/$1
         rm /etc/nginx/sites-available/$1
         rm /etc/nginx/sites-enabled/$1
-        cp /etc/nginx/cache.conf /etc/nginx/cache.conf-`date +%Y%M%d%H%M%S`
-        cat /etc/nginx/cache.conf | grep -v "$1" > /etc/nginx/cache.conf.new
-        mv /etc/nginx/cache.conf.new /etc/nginx/cache.conf
         nginx -s reload
     else
         echo "${1} does not exist."
