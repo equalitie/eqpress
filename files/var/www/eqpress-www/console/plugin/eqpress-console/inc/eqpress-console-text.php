cbf9810ad87ddfccd51143bcc5127c4b00865e11
RamJett <ramjett@ramnic.com>
2019-12-11 21:36:37 +0000

    update some text

--- files/var/www/eqpress-www/console/plugin/eqpress-console/inc/eqpress-console-text.php
+++ files/var/www/eqpress-www/console/plugin/eqpress-console/inc/eqpress-console-text.php
@@ -46,7 +46,7 @@ function ep_console_docs_manage_cache() {
 					<div class="postbox">
 						<h3><span>Manage Cache</span></h3>
 						<div class="inside">
-							<p>Manage Cache provides a way to delete the server cache. If you are making changes to your content and you need to see the changes immediately then you can use this feature to purge the web server's cache. It might take up to a minute for the cache to be removed depending on the its size.</p>
+							<p>Manage Cache provides a way to delete the server cache. If you are making changes to your content and you need to see the changes immediately, you can use this feature to purge the web server's cache. It might take up to a minute for the cache to be removed depending on the its size.</p>
 						</div> <!-- .inside -->
 					</div> <!-- .postbox -->
 				</div> <!-- .meta-box-sortables .ui-sortable -->
@@ -74,7 +74,7 @@ function ep_console_docs_view_logs() {
 							<p>View Logs allows you to view the following log files:</p>
 							<ol>
 								<li><strong>PHP error log</strong> - contains a record of all PHP errors produced by plugins and themes.</li>
-								<li><strong>Web server access log</strong> - contains a record of every file transfered from you site.</li>
+								<li><strong>Web server access log</strong> - contains a record of every file transferred from your site.</li>
 								<li><strong>Web server error log</strong> - contains a record of every error encountered by the web server.</li>
 							</ol>
 						</div> <!-- .inside -->
@@ -101,8 +101,8 @@ function ep_console_docs_file_permissions() {
 					<div class="postbox">
 						<h3><span>File Permissions</span></h3>
 						<div class="inside">
-							<p>The File Permissions feature allows you to reset the permissions and ownership on your files back to the default settings. The reason you might need to do this is if you upload or install a plugin manually and then that plugin requires write access to functions properly. The issue is that your SFTP username is not the same as the web server's user name so when you upload files into your document root you do so as your SFTP username. So, when the plugin tries to write to a file or directory that's owned by your SFTP username and not the web server's username it will fail because of insufficient ownership and permission of those files. The button on the File Permissions page will reset all directories and files under your document root to be owned by the web server user.</p>
-							<p>This default setting is very convenient for installing and updating plugins and themes but it's not the most secure way to configure a WordPress environment. This is why we also provide the Security Lockdown feature.
+							<p>The File Permissions feature allows you to reset the permissions and ownership on your files back to the default settings. The reason you might need to do this is if you upload or install a plugin manually and then that plugin requires write access to function properly. The issue is that your SFTP username is not the same as the web server's user name, so when you upload files into your document root you do so as your SFTP username. Therefore, when the plugin tries to write to a file or directory that is owned by your SFTP username and not the web server's username, it will fail because of insufficient ownership and permissions on those files. The button in the File Permissions page will reset all directories and files under your document root to be owned by the web server user.</p>
+							<p>This default setting is very convenient for installing and updating plugins and themes, but it's not the most secure way to configure a WordPress environment. This is why we also provide the Security Lockdown feature.
 						</div> <!-- .inside -->
 					</div> <!-- .postbox -->
 				</div> <!-- .meta-box-sortables .ui-sortable -->
@@ -127,9 +127,9 @@ function ep_console_docs_security_lockdown() {
 					<div class="postbox">
 						<h3><span>Security Lockdown</span></h3>
 						<div class="inside">
-							<p>The Security Lockdown feature allows you to change the ownership and permissions on all the files and directories under your document root to be owned by your SFTP username. When you choose to lock down your site none of your files will be owned by the web server's user which effectively prevents it from writing to any of your files or directories. This creates a very secure environment since access to your public site is via the web server and by disallowing the web server from having write access then you are protecting yourself against any potential hacks that work by creating or downloading new files within your document root.</p>
-							<p>When the site is locked down you will see the text "Site Locked Down" in your admin bar at the top of the page which is also a link to the Security Lockdown page.</p>
-							<p><strong>Important Note:</strong> When the site is locked down you will not be able to install new plugins or themes. You will not be able to update plugins, themes or WordPress itself. This is not a bug but a feature since the web server does not have write access to your files. This is exactly what the lockdown is supposed to do. If you need to update or install a plugin (or theme), simply unlock your site, peform the update or installation and then lock the site down once again.</p>
+							<p>The Security Lockdown feature allows you to change the ownership and permissions on all the files and directories under your document root to be owned by your SFTP username. When you choose to lock down your site, none of your files will be owned by the web server's user, which effectively prevents it from writing to any of your files or directories. This creates a very secure environment, since access to your public site is via the web server and by disallowing the web server from having write access, you are protecting yourself against any potential hacks that work by creating or downloading new files within your document root.</p>
+							<p>When the site is locked down, you will see the text "Site Locked Down" in your admin bar at the top of the page which is also a link to the Security Lockdown page.</p>
+							<p><strong>Important Note:</strong> When the site is locked down, you will not be able to install new plugins or themes. You will not be able to update plugins, themes or WordPress itself. This is not a bug but a feature, since the web server does not have write access to your files. This is exactly what the lockdown is supposed to do. If you need to update or install a plugin (or theme), simply unlock your site, peform the update or installation and then lock the site down once again.</p>
 						</div> <!-- .inside -->
 					</div> <!-- .postbox -->
 				</div> <!-- .meta-box-sortables .ui-sortable -->
@@ -179,14 +179,14 @@ function ep_console_docs_wpadmin_ssl() {
 					<div class="postbox">
 						<h3><span>Administration Over SSL</span></h3>
 						<div class="inside">
-							<p>SSL provides confidentiality between your browser and the web server. By encrypting the communication between you and the server you are making it very difficult for malicious hackers to steal your private information. Credentials such as usernames and passwords will be undecipherable if they were to be intercepted while in transit if you use SSL. Same with your authentication tokens such as cookies which are sent every time you view or make changes via the admin screens.</p>
+							<p>SSL provides confidentiality between your browser and the web server. By encrypting the communication between you and the server, you are making it very difficult for malicious hackers to steal your private information. If you use SSL, credentials such as usernames and passwords will be undecipherable if they were to be intercepted while in transit. Same with your authentication tokens such as the cookies which are sent every time you view or make changes via the admin screens.</p>
 							<p>There are 3 choices when configuring WordPress to use SSL:
 							<ol>
-								<li>Enable SSL for logins and all admin screens.</li>	
+								<li>Enable SSL for login and all admin screens.</li>	
 								<li>Enable SSL for logging in only.</li>	
 								<li>Disable SSL.</li>	
 							</ol></p>
-							<p>Please note that if you enable SSL you will be using the our SSL certificates therefore you will encounter SSL warnings the first time you visit your admin screens. You can read more about the warnings you will encounter on the help page titled <a href="http://{{ org_support_url }}/security-warning-for-wp-admin-and-ssl-certificates/" target="_blank" title="Security warning for wp-admin and SSL certificates">Security warning for wp-admin and SSL certificates.</a> If you have your own SSL certificates please get in touch with <a href="mailto:{{ org_support_email }}">Support</a> and we will install them for you.</p>
+							<p>Please note that if you enable SSL, you will be using our SSL certificates and therefore you will encounter SSL warnings the first time you visit your admin screens. If you have your own SSL certificates, please get in touch with the eQPress team through the Dashboard and we will install them for you.</p>
 							<p>You can read more about <a href="http://codex.wordpress.org/Administration_Over_SSL" target="_blank" title="Administration Over SSL for WordPress">Administration Over SSL</a> on the WordPress Codex</p>	
 					</div> <!-- .inside -->
 					</div> <!-- .postbox -->
@@ -218,7 +218,7 @@ function ep_console_docs_editor() {
 								<li>Enable plugin and theme editing via the admin screens.</li>	
 								<li>Disable plugin and theme editing via the admin screens.</li>	
 							</ol></p>
-							<p>You can read more about <a href="http://codex.wordpress.org/Editing_wp-config.php#Disable_the_Plugin_and_Theme_Editor" target="_blank" title=" Disable the Plugin and Theme Editor"> Disabling the Plugin and Theme Editor</a> on the WordPress Codex</p>	
+							<p>Read more about <a href="http://codex.wordpress.org/Editing_wp-config.php#Disable_the_Plugin_and_Theme_Editor" target="_blank" title=" Disable the Plugin and Theme Editor"> Disabling the Plugin and Theme Editor</a> on the WordPress Codex</p>	
 					</div> <!-- .inside -->
 					</div> <!-- .postbox -->
 				</div> <!-- .meta-box-sortables .ui-sortable -->
